25af967f0baa7d5355e6bb584e54b8e74cfef5d2
Nathan Goldbaum <nathan12343@gmail.com>
2020-12-05 10:45:44 -0700

    day 5

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,11 +1,33 @@
 use std::io::Read;
+use std::str::FromStr;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    let contents = get_contents("input.test")?;
+    let contents = get_contents("input")?;
 
-    println!("{}", contents);
+    let mut seats: Vec<Seat> = contents
+        .lines()
+        .map(|x| x.parse::<Seat>().unwrap())
+        .collect();
+
+    // part 1
+    let max_id = seats.iter().fold(0, |acc, x| std::cmp::max(acc, x.seat_id));
+
+    dbg!(max_id);
+
+    // part 2
+    seats.sort_by(|a, b| a.seat_id.partial_cmp(&b.seat_id).unwrap());
+    let open_seat = seats.windows(3).fold(0, |acc, x| {
+        if x[0].seat_id + 1 != x[1].seat_id {
+            x[1].seat_id - 1
+        } else {
+            acc
+        }
+    });
+
+    dbg!(open_seat);
 
     Ok(())
 }
@@ -18,4 +40,63 @@ fn get_contents(filename: &str) -> Result<String> {
     f.read_to_string(&mut contents)?;
 
     Ok(contents)
+}
+
+#[derive(Debug, PartialEq)]
+struct Seat {
+    row: usize,
+    col: usize,
+    seat_id: usize,
+}
+
+impl FromStr for Seat {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Seat> {
+        let boarding_pass = s.to_string();
+        let seat_id = usize::from_str_radix(
+            &boarding_pass
+                .replace(&(['F', 'L'][..]), "0")
+                .replace(&['B', 'R'][..], "1"),
+            2,
+        )?;
+        Ok(Seat {
+            row: seat_id / 8,
+            col: seat_id % 8,
+            seat_id: seat_id,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_row_col() {
+        assert!(
+            "BFFFBBFRRR".parse::<Seat>().unwrap()
+                == Seat {
+                    row: 70,
+                    col: 7,
+                    seat_id: 567,
+                }
+        );
+        assert!(
+            "FFFBBBFRRR".parse::<Seat>().unwrap()
+                == Seat {
+                    row: 14,
+                    col: 7,
+                    seat_id: 119,
+                }
+        );
+        assert!(
+            "BBFFBBFRLL".parse::<Seat>().unwrap()
+                == Seat {
+                    row: 102,
+                    col: 4,
+                    seat_id: 820,
+                }
+        );
+    }
 }
