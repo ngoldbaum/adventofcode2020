54e20d7ecf96aab0e4773fc49e5fea58bed9b46f
Nathan Goldbaum <nathan12343@gmail.com>
2020-12-11 15:39:48 -0700

    simplify

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,19 +2,19 @@ use anyhow::Result;
 
 use std::io::Read;
 
-struct Arena(Vec<Vec<Option<i32>>>);
+struct Arena(Vec<Vec<Option<usize>>>);
 
 impl Arena {
     fn step(&mut self) {
         let grid = &mut self.0;
-        let (nrow, ncol): (i32, i32) = (grid.len() as i32, grid[0].len() as i32);
+        let (nrow, ncol): (usize, usize) = (grid.len(), grid[0].len());
         let grid_indices = (0..nrow)
             .flat_map(|i| (0..ncol).map(move |j| (i, j)))
-            .collect::<Vec<(i32, i32)>>();
-        let counts: Vec<i32> = grid_indices
+            .collect::<Vec<(usize, usize)>>();
+        let counts: Vec<usize> = grid_indices
             .iter()
             .map(|&(i, j)| {
-                let mut iters: Vec<Box<dyn std::iter::Iterator<Item = (i32, i32)>>> = vec![
+                let mut iters: Vec<Box<dyn std::iter::Iterator<Item = (usize, usize)>>> = vec![
                     Box::new((0..i).rev().zip(std::iter::repeat(j))),
                     Box::new((i + 1..ncol).zip(std::iter::repeat(j))),
                     Box::new(std::iter::repeat(i).zip(j + 1..nrow)),
@@ -26,65 +26,39 @@ impl Arena {
                 ];
                 iters
                     .iter_mut()
-                    .map(
-                        |iter| match iter.find_map(|(ii, jj)| grid[ii as usize][jj as usize]) {
-                            Some(c) => c,
-                            None => 0,
-                        },
-                    )
+                    .map(|iter| match iter.find_map(|(ii, jj)| grid[ii][jj]) {
+                        Some(c) => c,
+                        None => 0,
+                    })
                     .sum()
             })
             .collect();
         grid_indices
             .into_iter()
             .zip(counts.into_iter())
-            .for_each(|((i, j), ac)| match grid[i as usize][j as usize] {
+            .for_each(|((i, j), ac)| match grid[i][j] {
                 Some(c) => {
                     if c == 0 && ac == 0 {
-                        grid[i as usize][j as usize] = Some(1);
+                        grid[i][j] = Some(1);
                     }
                     if c == 1 && ac > 4 {
-                        grid[i as usize][j as usize] = Some(0);
+                        grid[i][j] = Some(0);
                     }
                 }
                 None => (),
             });
     }
 
-    fn sum(&self) -> i32 {
+    fn sum(&self) -> usize {
         self.0
             .iter()
-            .map(|r| {
-                r.iter()
-                    .filter(|c| c.is_some())
-                    .map(|p| p.unwrap())
-                    .sum::<i32>()
+            .flat_map(|r| {
+                r.iter().map(|&p| match p {
+                    Some(c) => c,
+                    None => 0,
+                })
             })
             .sum()
-    }
-}
-
-impl std::fmt::Display for Arena {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            self.0
-                .iter()
-                .map(|l| l
-                    .iter()
-                    .map(|c| match c {
-                        Some(c) => match c {
-                            0 => 'L',
-                            1 => '#',
-                            _ => panic!(),
-                        },
-                        None => '.',
-                    })
-                    .collect::<String>())
-                .collect::<Vec<String>>()
-                .join("\n")
-        )
     }
 }
 
@@ -108,8 +82,6 @@ fn main() -> Result<()> {
     );
 
     loop {
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        println!("{}", grid);
         let last_step = grid.0.clone();
         grid.step();
 
